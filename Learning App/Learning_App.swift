57e7b7c73ee7da4bce77f69c7ad98ca8e442d6de
Anton Nagornyi <nagorny.anton@gmail.com>
2022-05-01 01:52:54 +0300

    – Rename ContentView to HomeView – Organise files into folders – Add ContentModel – Set ContentModel as EnvironmentObject

--- Learning App/Learning_App.swift
+++ Learning App/Learning_App.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 @main
-struct Learning_AppApp: App {
+struct Learning_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ Learning App/ModelViews/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  Learning App
+//
+//  Created by Anton Nagornyi on 01.05.2022.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
