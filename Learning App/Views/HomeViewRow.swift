4bf1170b19ae2b486d86e2463625356eab8511d8
Anton Nagornyi <nagorny.anton@gmail.com>
2022-05-03 19:06:13 +0300

    – Create HomeViewRow view – Program HomeView, make it show home view rows

---
+++ Learning App/Views/HomeViewRow.swift
@@ -0,0 +1,66 @@
+//
+//  HomeViewRow.swift
+//  Learning App
+//
+//  Created by Anton Nagornyi on 03.05.2022.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    
+    var image: String
+    var title: String
+    var description: String
+    var count: String
+    var time: String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+            HStack {
+                Image(image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                Spacer()
+                VStack (alignment: .leading, spacing: 10) {
+                    Text("Learn \(title)")
+                        .bold()
+                    Text(description)
+                        .padding(.bottom, 20)
+                        .font(Font.system(size: 15))
+                    HStack {
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 15, height: 15)
+                        Text(count)
+                            .font(Font.system(size: 12))
+//                        Spacer()
+                        Image(systemName: "clock")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 15, height: 15)
+                        Text(time)
+                            .font(Font.system(size: 12))
+                    }
+                    
+                }
+                Spacer()
+            }
+            .padding(.horizontal, 25)
+        }
+    }
+}
+
+struct HomeViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewRow(image: "swift", title: "Learn SwiftUI", description: "Some description", count: "20 Lessons", time: "3 Hours")
+    }
+}
